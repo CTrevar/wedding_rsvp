70103b71e51603cde52034e47ed0fb0e26d0b046
CTrevar <cristina.trevar@gmail.com>
2017-04-23 13:37:27 -0500

    It's starting to take shape

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,9 @@
 class WelcomeController < ApplicationController
   layout "main"
+
+  def index
+    @pretty_place = "#{ENV['wedding_city']}, #{ENV['wedding_state']}"
+    @pretty_date = Date.parse(ENV['wedding_date']).strftime("%A, %d %B %Y")
+    @acommodations = Hotel.all
+  end
 end
