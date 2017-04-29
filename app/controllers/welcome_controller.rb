e939ece95fcaf15aca569b5887d3b52803a5673c
CTrevar <cristina.trevar@gmail.com>
2017-04-29 11:37:13 -0500

    translations for dates, set default locale to es

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,8 @@ class WelcomeController < ApplicationController
 
   def index
     @pretty_place = "#{ENV['wedding_city']}, #{ENV['wedding_state']}"
-    @pretty_date = Date.parse(ENV['wedding_date']).strftime("%A, %d %B %Y")
+    @pretty_date = Date.parse(ENV['wedding_date'])
+    @pretty_date = I18n.localize(@pretty_date, format: '%A, %d %B %Y')
     @acommodations = Hotel.all
     @registries = Registry.all
     @itinerary = Activity.all.group_by(&:start_day)
